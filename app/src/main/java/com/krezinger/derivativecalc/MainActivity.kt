6c4e4edd6b18cf192487011bebf6806bcfe8d042
Christopher <christopher-tibor.krezinger@student.uni-tuebingen.de>
2025-07-30 19:31:43 +0200

    Changed to 2K mode because of insatbility of IDE, made fun for buttons.

--- app/src/main/java/com/krezinger/derivativecalc/MainActivity.kt
+++ app/src/main/java/com/krezinger/derivativecalc/MainActivity.kt
@@ -7,15 +7,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -32,6 +38,7 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.krezinger.derivativecalc.ui.theme.DerivativecalcTheme
@@ -55,6 +62,24 @@ val MenuList: Array<String> = arrayOf(
     "About Me"
 )
 
+@Composable
+fun ClickableButton(title: String,
+                    color: Color = Color.Transparent,
+                    modifier: Modifier = Modifier,
+                    onClick: () -> Unit ){
+    /* somehow needs to be more scalable for enter button */
+    Card(colors = CardDefaults.cardColors(containerColor = color))
+     {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .clickable{onClick}
+        ) {
+            Text(modifier = modifier,text = title, color = MaterialTheme.colorScheme.onPrimaryContainer)
+        }
+
+     }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListInMenu() {
@@ -63,44 +88,20 @@ fun ListInMenu() {
                 WindowInsets.systemBars.asPaddingValues().calculateTopPadding()),
         drawerContainerColor = MaterialTheme.colorScheme.onPrimary,
         windowInsets = WindowInsets(0)
-    )
-            {
-        MenuList.forEachIndexed{
-            index, itemText -> Text(
-                text = itemText,
-                modifier = Modifier.padding(8.dp),
-                color = MaterialTheme.colorScheme.onPrimaryContainer
-            )
+    ) {
+        MenuList.forEachIndexed { index, itemText ->
+            ClickableButton(
+                title = itemText,
+                modifier = Modifier.padding(30.dp)
+            ) {
+                //needs UIlogik for About me and History
+            }
         }
 
     }
 }
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Composable
-fun MainScreenWithDrawer() {
-    val drawerState: DrawerState = rememberDrawerState(DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
 
-    ModalNavigationDrawer(
-        drawerContent = { ListInMenu() },
-        drawerState = drawerState
-    ){
-        Scaffold(topBar = {
-            TopBar {
-                scope.launch {
-                    if (drawerState.isClosed) {
-                        drawerState.open()
-                    } else {
-                        drawerState.close()
-                    }
-                }
-            }
-        },
-        content = { MainContainer() }
-        )
-    }
-}
 
 @Composable
 fun MainContainer(){
@@ -125,8 +126,33 @@ fun TopBar(title: String = "", onMenuClick: () -> Unit){
     )
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreenWithDrawer() {
+    val drawerState: DrawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+    ModalNavigationDrawer(
+        drawerContent = { ListInMenu() },
+        drawerState = drawerState
+    ){
+        Scaffold(topBar = {
+            TopBar {
+                scope.launch {
+                    if (drawerState.isClosed) {
+                        drawerState.open()
+                    } else {
+                        drawerState.close()
+                    }
+                }
+            }
+        },
+            content = { MainContainer() }
+        )
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
 fun AppPreview() {
     DerivativecalcTheme(dynamicColor = false) {
